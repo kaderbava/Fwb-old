83d7c801e0183a05f17914fa33088b406cd02115
npjohnson <johnsonnolen@gmail.com>
2021-02-27 20:43:03 +0700

    vendor: soong: Add TARGET_LEGACY_HW_DISK_ENCRYPTION conditional
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -46,6 +46,7 @@ SOONG_CONFIG_ancientNvidiaVars += \
 
 SOONG_CONFIG_NAMESPACES += ancientQcomVars
 SOONG_CONFIG_ancientQcomVars += \
+    legacy_hw_disk_encryption \
     should_wait_for_qsee \
     supports_extended_compress_format \
     supports_hw_fde \
@@ -68,6 +69,7 @@ SOONG_CONFIG_ancientGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IG
 SOONG_CONFIG_ancientGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
 SOONG_CONFIG_ancientGlobalVars_target_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
 SOONG_CONFIG_ancientNvidiaVars_uses_nv_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
+SOONG_CONFIG_ancientQcomVars_legacy_hw_disk_encryption := $(TARGET_LEGACY_HW_DISK_ENCRYPTION)
 SOONG_CONFIG_ancientQcomVars_should_wait_for_qsee := $(TARGET_KEYMASTER_WAIT_FOR_QSEE)
 SOONG_CONFIG_ancientQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_ancientQcomVars_supports_hw_fde := $(TARGET_HW_DISK_ENCRYPTION)
