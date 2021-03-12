babb2309cd2dd35c1852190d6a041c0d43386eff
xyzuan <i.xyzuan@gmail.com>
2021-03-12 14:13:28 +0700

    vendor: nuke themed_bootanimation.
    it no longer work properly.

--- config/common.mk
+++ config/common.mk
@@ -127,14 +127,6 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI \
     NexusLauncherRelease
 
-# Themed bootanimation
-TARGET_MISC_BLOCK_OFFSET ?= 0
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.misc.block.offset=$(TARGET_MISC_BLOCK_OFFSET)
-PRODUCT_PACKAGES += \
-    misc_writer \
-    themed_bootanimation
-
 # Branding
 include vendor/ancient/config/branding.mk
 
