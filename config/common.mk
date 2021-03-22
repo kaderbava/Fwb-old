a0f63933d77b6871b33511c20863f57e8edf441f
Bruno Martins <bgcngm@gmail.com>
2021-03-22 21:57:55 +0700

    vendor: Whitelist some of our own broadcast actions
     * Starting with Oreo, all the apps that target O+ are always subject to
       background restrictions (aosp-mirror/platform_frameworks_base@42a386b).
    
       The manifest-registered receivers of apps with a targetSdkVersion
       higher than 25 won't receive the broadcast. Instead, a message similar
       to the following will appear in LogCat:
    
           10-07 14:13:49.594   719   736 W BroadcastQueue: Background execution not allowed: receiving Intent { act=lineageos.intent.action.LID_STATE_CHANGED flg=0x20000010 (has extras) } to org.lineageos.flipflap/.EventReceiver
    
     * Exempt some of our broadcast actions so that we can keep targetting
       the latest SDK.
    
    Change-Id: I075608e18994f64db5e89658757dca307f07c417

--- config/common.mk
+++ config/common.mk
@@ -57,6 +57,7 @@ PRODUCT_COPY_FILES += \
 
 # Some permissions
 PRODUCT_COPY_FILES += \
+    vendor/ancient/config/permissions/ancient-sysconfig.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/ancient-sysconfig.xml \
     vendor/ancient/config/permissions/backup.xml:system/etc/sysconfig/backup.xml \
     vendor/ancient/config/permissions/privapp-permissions-system-ancient.xml:system/etc/permissions/privapp-permissions-system-ancient.xml \
     vendor/ancient/config/permissions/privapp-permissions-product-ancient.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-product-ancient.xml \
