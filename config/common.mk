cc64d4d870d1135c707e7534fe319edd60c30dcd
ArianK16a <ariank16a@gmail.com>
2021-10-13 15:23:59 +0700

    vendor: config: Enable support of one-handed mode

--- config/common.mk
+++ config/common.mk
@@ -180,6 +180,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.sf.blurs_are_expensive=1 \
     ro.surface_flinger.supports_background_blur=1
 
+# Enable support of one-handed mode
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.support_one_handed_mode=true
+
 # Customization
 #include vendor/google-customization/config.mk
 
