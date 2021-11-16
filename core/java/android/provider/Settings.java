d7dac7182d40d7f449069edb62ee9f328a562dda
maitreya29 <maitreyapatni30@gmail.com>
2021-11-17 02:25:12 +0700

    base: Battery Saver Profiles [1/2]
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5920,6 +5920,12 @@ public final class Settings {
         public static final String CHARGING_ANIMATION = "charging_animation";
 
         /**
+         * Battery saver profiles
+         * @hide
+         */
+        public static final String POWER_PROFILE_TYPE = "power_profile_type";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
